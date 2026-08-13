db23ae5299555a851ee2866fdd02b60df7346f40
Santiago <santitassara@gmail.com>
2026-08-13 12:27:02 -0300

    feat(backend): agregar endpoints PATCH /usuarios/me/perfil y GET /usuarios/:uid/perfil

--- backend/src/routes/usuariosRoutes.js
+++ backend/src/routes/usuariosRoutes.js
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get('/sancionados', verificarToken, verificarAdmin, envolverAsync(usuariosController.listarSancionados));
 router.post('/:uid/perdonar', verificarToken, verificarAdmin, envolverAsync(usuariosController.perdonar));
 router.patch('/me/posiciones', verificarToken, envolverAsync(usuariosController.actualizarMisPosiciones));
+router.patch('/me/perfil', verificarToken, envolverAsync(usuariosController.actualizarMiPerfil));
+router.get('/:uid/perfil', verificarToken, envolverAsync(usuariosController.obtenerPerfilDeJugador));
 
 module.exports = router;
